7ebc27f64261bf4a6a6a11e8a59e6de724e8fd87
Liblume <Liblume890726@gmail.com>
2019-05-23 17:29:59 +0800

    base finished

---
+++ index.js
@@ -0,0 +1,27 @@
+const LineBotSDK = require("@line/bot-sdk");
+
+const channelSecret = process.env.secret;
+
+const koa = require("koa");
+const koaBodyparser = require("koa-bodyparser");
+const koaRouter = require("koa-router");
+
+const lineAPI = new LineBotSDK.LineBotApi(process.env.channelAccessToken);
+
+const app = new koa();
+const router = new koaRouter();
+
+app.use(koaBodyparser());
+
+router.post("/",function(ctx) {
+    if (LineBotSDK.validateSignature(ctx.request.body, channelSecret)){
+        ctx.status = 200;
+    }else {
+        ctx.status = 401;
+        ctx.body = "Authorize failed .";
+    }
+});
+
+app.use(router.routes());
+
+const server = app.listen(process.env.PORT || 8080);
