acc92f06b3e91aef2dad67a239c8a12887ff7adc
Liblume <Liblume890726@gmail.com>
2019-05-23 17:51:02 +0800

    error debug

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const LineBotSDK = require("@line/bot-sdk");
 
-const channelSecret = process.env.secret;
-
 const koa = require("koa");
 const koaBodyparser = require("koa-bodyparser");
 const koaRouter = require("koa-router");
 
-const lineAPI = new LineBotSDK.LineBotApi(process.env.channelAccessToken);
+const channelSecret = process.env.channelSecret;
+const channelAccessToken = process.env.channelAccessToken;
+
+const lineAPI = new LineBotSDK.Client({
+  "channelSecret": channelSecret,
+  "channelAccessToken": channelAccessToken
+});
 
 const app = new koa();
 const router = new koaRouter();
 
 app.use(koaBodyparser());
 
-router.post("/",function(ctx) {
-    if (LineBotSDK.validateSignature(ctx.request.body, channelSecret)){
-        ctx.status = 200;
-        ctx.request.body.events.map(MessageHandler);
-    }else {
-        ctx.status = 401;
-        ctx.body = "Authorize failed .";
-    }
+router.post("/", function (ctx) {
+  if (LineBotSDK.validateSignature(ctx.request.rawBody, channelSecret, ctx.request.headers['x-line-signature'])) {
+    ctx.status = 200;
+    ctx.request.body.events.map(MessageHandler);
+  } else {
+    ctx.status = 401;
+    ctx.body = "Authorize failed.";
+  }
 });
 
 app.use(router.routes());
@@ -28,5 +32,5 @@ app.use(router.routes());
 const server = app.listen(process.env.PORT || 8080);
 
 async function MessageHandler(event) {
-    console.loh(event);
+  console.log(event);
 }
